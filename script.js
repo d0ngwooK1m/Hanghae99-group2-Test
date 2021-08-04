21e621df035a75d3e4b2ddf6aa7016adc6e33d84
CH_Hwang <hch950627@naver.com>
2021-08-04 18:36:49 +0900

    follow box

--- script.js
+++ script.js
@@ -8,11 +8,16 @@ function HideBox(){
     F_img.style.display = 'none';
 }
 
-function FollowMouse(){
-    F_img.style.left = positionLeft - 10 + "px";
-    F_img.style.left = positionTop - 10 + "px";
+function FollowMouse(e){
+    F_img.style.left = e.clientX +"px";
+    F_img.style.top = e.clientY +"px";
 }
 
 img_bg.addEventListener('mouseover',ShowBox)
+F_img.addEventListener('mouseover',ShowBox)
 img_bg.addEventListener('mouseout',HideBox)
+F_img.addEventListener('mouseout',HideBox)
+
+
+img_bg.addEventListener('mousemove',FollowMouse)
 F_img.addEventListener('mousemove',FollowMouse)
